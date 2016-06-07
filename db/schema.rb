23488cd5830c417b8b21cd85eea6e60fb637ddd6
Simone Taeggi <startae14@gmail.com>
2016-06-07 17:45:22 +0200

    modified seed.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607153418) do
+ActiveRecord::Schema.define(version: 20160607154117) do
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160607153418) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "login"
+    t.string   "pass"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 Person.destroy_all
 
 Person.create! [
-    { first_name: "Kalman", last_name: "Smith", age: 33 },
-    { first_name: "John", last_name: "Whatever", age: 27 },
-    { first_name: "Michael", last_name: "Smith", age: 15 },
-    { first_name: "Josh", last_name: "Oreck", age: 57 },
-    { first_name: "John", last_name: "Smith", age: 27 },
-    { first_name: "Bill", last_name: "Gates", age: 75 },
-    { first_name: "LeBron", last_name: "James", age: 30 }
+    { first_name: "Kalman", last_name: "Smith", age: 33, login: "kman", pass: "abc123" },
+    { first_name: "John", last_name: "Whatever", age: 27, login: "john1", pass: "123abc" },
+    { first_name: "Michael", last_name: "Smith", age: 15, login: "mike", pass: "not_telling" },
+    { first_name: "Josh", last_name: "Oreck", age: 57, login: "josh", pass: "password1" },
+    { first_name: "John", last_name: "Smith", age: 27, login: "john2", pass: "no_idea" },
+    { first_name: "Bill", last_name: "Gates", age: 75, login: "bill", pass: "windows3.1" },
+    { first_name: "LeBron", last_name: "James", age: 30, login: "bron", pass: "need more rings" }
 ]
