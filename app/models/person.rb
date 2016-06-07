e7c0b06202ee93143eb069f2b7bbd7835cf2653d
Simone Taeggi <startae14@gmail.com>
2016-06-07 17:48:36 +0200

    added personal_info

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+    # aggiungo relazione con personal_info
+    has_one: personal_info
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607154117) do
+ActiveRecord::Schema.define(version: 20160607154641) do
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20160607154117) do
     t.string   "login"
     t.string   "pass"
   end
+
+  create_table "personal_infos", force: :cascade do |t|
+    t.float    "height"
+    t.float    "weight"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "personal_infos", ["person_id"], name: "index_personal_infos_on_person_id"
 
 end
