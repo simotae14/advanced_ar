7077778ac2c46e409310ec51dce44d36a028268b
Simone Taeggi <startae14@gmail.com>
2016-06-07 17:40:18 +0200

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Person.destroy_all
+
+Person.create! [
+    { first_name: "Kalman", last_name: "Smith", age: 33 },
+    { first_name: "John", last_name: "Whatever", age: 27 },
+    { first_name: "Michael", last_name: "Smith", age: 15 },
+    { first_name: "Josh", last_name: "Oreck", age: 57 },
+    { first_name: "John", last_name: "Smith", age: 27 },
+    { first_name: "Bill", last_name: "Gates", age: 75 },
+    { first_name: "LeBron", last_name: "James", age: 30 }
+]
